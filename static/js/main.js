f293c04229289e02c033c0b8bf31c2e6ffd430dc
giusdp <dpl.gsp@tutanota.com>
2022-10-25 10:11:34 +0200

    fix: move theme set in base

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,3 @@ document.getElementById('mode').addEventListener('click', () => {
   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 });
 
-// enforce local storage setting but also fallback to user-agent preferences
-if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  document.body.classList.add('dark');
-}
